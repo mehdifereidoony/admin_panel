e612e2dee40275648959db4e6fab7d2e1160c9e5
mehdi <webtnt.ir@gmail.com>
2026-06-07 22:40:32 +0430

    feat: implement login form with validation

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,5 +1,67 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { loginSchema } from "../../schema/authSchema";
+
 const Login = () => {
-  return <p>Login</p>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+    shouldFocusError: true,
+    shouldUnregister: true,
+  });
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="container min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="card shadow border-0" style={{ width: "420px" }}>
+        <div className="card-body p-4">
+          <h3 className="text-center mb-4">ورود به پنل مدیریت</h3>
+
+          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+              <label className="form-label">نام کاربری</label>
+
+              <input
+                {...register("identity")}
+                type="text"
+                className="form-control"
+                placeholder=" نام کاربری یا ایمیل"
+              />
+              {errors.identity && (
+                <p className="text-danger mt-1">{errors.identity.message}</p>
+              )}
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">رمز عبور</label>
+
+              <input
+                {...register("password")}
+                type="password"
+                className="form-control"
+                placeholder="رمز عبور"
+              />
+              {errors.password && (
+                <p className="text-danger mt-1">{errors.password.message}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              disabled={!isDirty || !isValid || isSubmitting}
+              className="btn btn-primary w-100"
+            >
+              ورود
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/schema/authSchema.js
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  identity: z
+    .string()
+    .trim()
+    .min(1, "این فیلد اجباری هست")
+    .refine(
+      (value) => {
+        const isEmail = z.string().email().safeParse(value).success;
+        const isUserName = /^[a-zA-Z0-9_]{3,30}$/.test(value);
+        return isEmail || isUserName;
+      },
+      {
+        message: "نام کاربری یا ایمیل نامعتبر",
+      }
+    ),
+  password: z.string().min(6, "حداقل 6 کارکتر").max(99, "حداکثر 99 کارکتر"),
+});
